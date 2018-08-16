d672547498f7c58ecef11cad0e41fab52b0dce97
Vyhdra <joebobjrjr@gmail.com>
2018-08-16 18:39:26 -0400

    Door Button
    Door Controller Code for button. Will make example soon

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour {
 
+
+    public GameObject Door;
+    public bool doorIsOpening;
+    public float doorOpenY;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+
+        if (doorIsOpening == true)
+        {
+            Door.transform.Translate(Vector3.up * Time.deltaTime * 5);
+            //if the bool is true, open the door, you dumb shitting program
+
+        }
+
+        if (Door.transform.position.y > doorOpenY)
+        {
+            doorIsOpening = false;
+        }
+
+
+    }
+
+    void OnMouseDown()
+    {
+        doorIsOpening = true;
+        // if we click on the button, door must start to open???? (Switch this for a 'press f to door'), make the box collider a trigger
+    }
 }
